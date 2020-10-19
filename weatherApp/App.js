3d23e97139546e0947b562df01e2f3c2f4b0ae4b
FaziletKosure <faziletkosure1@gmail.com>
2020-10-19 21:40:27 +0200

    add styles

--- weatherApp/App.js
+++ weatherApp/App.js
@@ -4,22 +4,21 @@ import {
   Text,
   SafeAreaView,
   Dimensions,
-  StyleSheet,
+  mainheet,
   Image,
   ImageBackground,
   StatusBar,
   TouchableOpacity,
   TextInput,
   ActivityIndicator,
+  KeyboardAvoidingView,
 } from 'react-native';
 
+import {main} from './src/styles';
 import Config from 'react-native-config';
 import Loader from './src/Loading';
 
 // Config.GOOGLE_MAPS_API_KEY; // 'abcdefgh'
-
-const HEIGHT = Dimensions.get('window').height;
-const WIDTH = Dimensions.get('window').width;
 
 import Icon from 'react-native-vector-icons/dist/FontAwesome';
 
@@ -84,168 +83,69 @@ const App = () => {
   }, [appState.city]);
 
   return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar translucent={true} backgroundColor="#000" />
-      <ImageBackground
-        source={require('./src/assets/out.png')}
-        style={styles.Image_Background_Style}>
-        <View style={styles.Search_Box_View}>
-          <TextInput
-            keyboardAppearance="dark"
-            placeholder="Search"
-            placeholderTextColor="#FFF"
-            style={styles.Search_Box}
-            value={appState.city}
-            onChangeText={(text) => setAppState({...appState, city: text})}
-          />
-          <TouchableOpacity style={styles.button_touch} onPress={fetch_weather}>
-            <Icon name="search" size={24} color="#FFF" />
-          </TouchableOpacity>
-        </View>
-
-        <View style={styles.Weather_Box_Main}>
-          <View style={styles.Weather_Holder_View}>
-            <Image
-              tintColor="#FFF"
-              source={{
-                uri:
-                  'http://openweathermap.org/img/wn/' +
-                  appState.icon +
-                  '@2x.png',
-              }}
-              style={styles.Weather_Image}
+    <SafeAreaView style={main.container}>
+      <KeyboardAvoidingView>
+        <StatusBar translucent={true} backgroundColor="#000" />
+        <ImageBackground
+          source={require('./src/assets/out.png')}
+          style={main.Image_Background_Style}>
+          <View style={main.Search_Box_View}>
+            <TextInput
+              keyboardAppearance="dark"
+              placeholder="Search"
+              placeholderTextColor="#FFF"
+              style={main.Search_Box}
+              value={appState.city}
+              onChangeText={(text) => setAppState({...appState, city: text})}
             />
-            <View>
-              <Text style={styles.temprature_text}>{appState.temp}</Text>
-              <Text style={styles.city_text}>{appState.city_display}</Text>
-              <Text style={styles.city_text}>Sunrise:{appState.sunrise}</Text>
-            </View>
+            <TouchableOpacity style={main.button_touch} onPress={fetch_weather}>
+              <Icon name="search" size={24} color="#FFF" />
+            </TouchableOpacity>
           </View>
-        </View>
 
-        {appState.isLoading ? (
-          <Loader />
-        ) : (
-          <View style={styles.Info_Box_View}>
-            <View style={styles.Info_Holder_Veiw}>
-              <Text style={styles.Main_Weather_Text}>{appState.main}</Text>
-              <Text style={styles.description_text}>{appState.desc}</Text>
-              <Text style={styles.humidity_text}>
-                Humidity : {appState.humidity}
-              </Text>
-              <Text style={styles.other_text}>
-                Pressure : {appState.pressure}
-              </Text>
-              <Text style={styles.other_text}>
-                Visibility : {appState.visibility}
-              </Text>
+          <View style={main.Weather_Box_Main}>
+            <View style={main.Weather_Holder_View}>
+              <Image
+                tintColor="#FFF"
+                source={{
+                  uri:
+                    'http://openweathermap.org/img/wn/' +
+                    appState.icon +
+                    '@2x.png',
+                }}
+                style={main.Weather_Image}
+              />
+              <View>
+                <Text style={main.temprature_text}>{appState.temp}</Text>
+                <Text style={main.city_text}>{appState.city_display}</Text>
+                <Text style={main.city_text}>Sunrise:{appState.sunrise}</Text>
+              </View>
             </View>
           </View>
-        )}
-      </ImageBackground>
+
+          {appState.isLoading ? (
+            <Loader />
+          ) : (
+            <View style={main.Info_Box_View}>
+              <View style={main.Info_Holder_Veiw}>
+                <Text style={main.Main_Weather_Text}>{appState.main}</Text>
+                <Text style={main.description_text}>{appState.desc}</Text>
+                <Text style={main.humidity_text}>
+                  Humidity : {appState.humidity}
+                </Text>
+                <Text style={main.other_text}>
+                  Pressure : {appState.pressure}
+                </Text>
+                <Text style={main.other_text}>
+                  Visibility : {appState.visibility}
+                </Text>
+              </View>
+            </View>
+          )}
+        </ImageBackground>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 };
 
 export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    height: HEIGHT,
-    width: WIDTH,
-  },
-  Image_Background_Style: {
-    height: '100%',
-    width: '100%',
-  },
-  Search_Box_View: {
-    height: '20%',
-    width: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
-    flexDirection: 'row',
-  },
-  Search_Box: {
-    height: '35%',
-    width: '80%',
-    borderColor: '#FFF',
-    borderWidth: 1,
-    borderRadius: 15,
-    color: '#FFF',
-    paddingHorizontal: 15,
-  },
-  button_touch: {
-    marginLeft: '5%',
-    height: '35%',
-    width: '8%',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  Weather_Box_Main: {
-    height: '30%',
-    width: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
-    flexDirection: 'row',
-  },
-  Weather_Holder_View: {
-    height: '80%',
-    width: '90%',
-    backgroundColor: 'rgba(255, 255, 255, 0.3)',
-    borderRadius: 15,
-    alignItems: 'center',
-    flexDirection: 'row',
-  },
-  Weather_Image: {
-    height: '80%',
-    width: '50%',
-  },
-  temprature_text: {
-    fontSize: 30,
-    color: '#FFF',
-    marginLeft: '5%',
-  },
-  city_text: {
-    fontSize: 20,
-    color: '#FFF',
-    marginLeft: '5%',
-    marginTop: '3%',
-  },
-  Info_Box_View: {
-    height: '45%',
-    width: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  Info_Holder_Veiw: {
-    height: '80%',
-    width: '90%',
-    backgroundColor: 'rgba(255, 255, 255, 0.92)',
-    borderRadius: 15,
-  },
-  Main_Weather_Text: {
-    fontSize: 28,
-    color: '#464646',
-    marginLeft: '8%',
-    marginTop: '8%',
-    fontWeight: 'bold',
-  },
-  description_text: {
-    fontSize: 20,
-    color: '#121212',
-    marginLeft: '8%',
-    marginTop: '3%',
-  },
-  humidity_text: {
-    fontSize: 18,
-    color: '#121212',
-    marginLeft: '8%',
-    marginTop: '5%',
-  },
-  other_text: {
-    fontSize: 18,
-    color: '#121212',
-    marginLeft: '8%',
-    marginTop: '2%',
-  },
-});
---
+++ weatherApp/src/styles.js
@@ -0,0 +1,112 @@
+import {StyleSheet, Dimensions} from 'react-native';
+
+/**
+ * #303e45
+ * #a7b6bd
+ * #4a636e
+ * #ff9b31
+ */
+const HEIGHT = Dimensions.get('window').height;
+const WIDTH = Dimensions.get('window').width;
+const main = StyleSheet.create({
+  container: {
+    height: HEIGHT,
+    width: WIDTH,
+  },
+  Image_Background_Style: {
+    height: '100%',
+    width: '100%',
+  },
+  Search_Box_View: {
+    height: '20%',
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  Search_Box: {
+    height: '35%',
+    width: '80%',
+    borderColor: '#FFF',
+    borderWidth: 1,
+    borderRadius: 15,
+    color: '#FFF',
+    paddingHorizontal: 15,
+  },
+  button_touch: {
+    marginLeft: '5%',
+    height: '35%',
+    width: '8%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  Weather_Box_Main: {
+    height: '30%',
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  Weather_Holder_View: {
+    height: '80%',
+    width: '90%',
+    backgroundColor: 'rgba(255, 255, 255, 0.3)',
+    borderRadius: 15,
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  Weather_Image: {
+    height: '80%',
+    width: '50%',
+  },
+  temprature_text: {
+    fontSize: 30,
+    color: '#FFF',
+    marginLeft: '5%',
+  },
+  city_text: {
+    fontSize: 20,
+    color: '#FFF',
+    marginLeft: '5%',
+    marginTop: '3%',
+  },
+  Info_Box_View: {
+    height: '45%',
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  Info_Holder_Veiw: {
+    height: '80%',
+    width: '90%',
+    backgroundColor: 'rgba(255, 255, 255, 0.92)',
+    borderRadius: 15,
+  },
+  Main_Weather_Text: {
+    fontSize: 28,
+    color: '#464646',
+    marginLeft: '8%',
+    marginTop: '8%',
+    fontWeight: 'bold',
+  },
+  description_text: {
+    fontSize: 20,
+    color: '#121212',
+    marginLeft: '8%',
+    marginTop: '3%',
+  },
+  humidity_text: {
+    fontSize: 18,
+    color: '#121212',
+    marginLeft: '8%',
+    marginTop: '5%',
+  },
+  other_text: {
+    fontSize: 18,
+    color: '#121212',
+    marginLeft: '8%',
+    marginTop: '2%',
+  },
+});
+
+export {main};
